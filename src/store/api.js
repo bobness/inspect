4027ed07c08752f8ef7fe29f23c05f0c3a3d2a2d
Steve Herzog <herzogdev@gmail.com>
2022-06-09 16:35:59 -0700

    updated api endpoint for server

--- src/store/api.js
+++ src/store/api.js
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
-// const baseUrl = 'http://18.116.14.240/api';
-const baseUrl = 'http://4b82-66-154-105-197.ngrok.io';
+const baseUrl = 'http://18.116.14.240/api';
+// const baseUrl = 'http://4b82-66-154-105-197.ngrok.io';
 
 const instance = axios.create({ baseURL: baseUrl, headers: { 'Content-Type': 'application/json' } });
 
