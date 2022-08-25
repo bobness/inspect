cda8538fc08045df0faa69c7d62c0a9e3b08ad10
Steve Herzog <herzogdev@gmail.com>
2022-08-24 19:49:16 -0500

    Added some params

--- ios/inspect_share/ShareViewController.swift
+++ ios/inspect_share/ShareViewController.swift
@@ -49,6 +49,7 @@ class ShareViewController: SLComposeServiceViewController {
  
  override func didSelectPost() {
        print("didSelectPost");
+   return true;
    }
 
  override func configurationItems() -> [Any]! {
