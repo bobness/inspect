f8da7c872c5df486cca4d1fc63d5e170834adee1
Steve Herzog <herzogdev@gmail.com>
2022-08-30 20:04:09 -0500

    fixed web url sharing feature

--- ios/inspect_share/ShareViewController.swift
+++ ios/inspect_share/ShareViewController.swift
@@ -14,6 +14,7 @@ class ShareViewController: SLComposeServiceViewController {
  let imageContentType = kUTTypeImage as String
  let videoContentType = kUTTypeMovie as String
  let textContentType = kUTTypeText as String
+ let plainTextContentType = kUTTypePlainText as String
  let urlContentType = kUTTypeURL as String
  let fileURLType = kUTTypeFileURL as String;
  
@@ -48,8 +49,15 @@ class ShareViewController: SLComposeServiceViewController {
  }
  
  override func didSelectPost() {
-   self.redirectToHostApp(type: .text)
+   print("handleText");
+   if let content = extensionContext!.inputItems[0] as? NSExtensionItem {
+     if let contents = content.attachments {
+       for (index, attachment) in (contents).enumerated() {
+         handlePlainText(content: content, attachment: attachment, index: index)
+       }
+     }
    }
+ }
 
  override func configurationItems() -> [Any]! {
    // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
@@ -76,6 +84,27 @@ class ShareViewController: SLComposeServiceViewController {
      }
    }
  }
+  
+  private func handlePlainText (content: NSExtensionItem, attachment: NSItemProvider, index: Int) {
+    attachment.loadItem(forTypeIdentifier: plainTextContentType, options: nil) { [weak self] data, error in
+      
+      if error == nil, let item = data as? String, let this = self {
+        
+        this.sharedText.append(item)
+        
+        // If this is the last item, save imagesData in userDefaults and redirect to host app
+        if index == (content.attachments?.count)! - 1 {
+          let userDefaults = UserDefaults(suiteName: "group.\(this.hostAppBundleIdentifier)")
+          userDefaults?.set(this.sharedText, forKey: this.sharedKey)
+          userDefaults?.synchronize()
+          this.redirectToHostApp(type: .text)
+        }
+        
+      } else {
+        self?.dismissWithError()
+      }
+    }
+  }
  
  private func handleUrl (content: NSExtensionItem, attachment: NSItemProvider, index: Int) {
    attachment.loadItem(forTypeIdentifier: urlContentType, options: nil) { [weak self] data, error in
@@ -83,7 +112,7 @@ class ShareViewController: SLComposeServiceViewController {
      if error == nil, let item = data as? URL, let this = self {
        
        this.sharedText.append(item.absoluteString)
-       
+       print(item.absoluteString);
        // If this is the last item, save imagesData in userDefaults and redirect to host app
        if index == (content.attachments?.count)! - 1 {
          let userDefaults = UserDefaults(suiteName: "group.\(this.hostAppBundleIdentifier)")
