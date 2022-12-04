1f5fb7d07ea239c33ffc92c33fa2cbb09545cc1a
Bob Stark <bob@datagotchi.net>
2022-12-03 16:43:46 -0800

    Fix AuthorViewScreen logos

--- src/pages/AuthorViewScreen.tsx
+++ src/pages/AuthorViewScreen.tsx
@@ -16,6 +16,8 @@ import { ListItem, Avatar, Button, Icon } from "react-native-elements";
 import BottomToolbar from "../components/BottomToolbar";
 import { getAuthUser, getProfileInformation } from "../store/auth";
 import { followAuthor, unfollowAuthor } from "../store/news";
+import NewsRow from "../components/NewsRow";
+import { Source } from "../types";
 
 export default function AuthorViewScreen(props: any) {
   const {
@@ -60,30 +62,38 @@ export default function AuthorViewScreen(props: any) {
   }, [userData]);
 
   const renderSummaryItem = ({ item }: any) => (
-    <ListItem
-      bottomDivider
-      hasTVPreferredFocus={undefined}
-      tvParallaxProperties={undefined}
-      style={{ flex: 1, width: "100%" }}
+    // <ListItem
+    //   bottomDivider
+    //   hasTVPreferredFocus={undefined}
+    //   tvParallaxProperties={undefined}
+    //   style={{ flex: 1, width: "100%" }}
+    //   onPress={() => {
+    //     navigation.navigate("NewsView", { data: item });
+    //   }}
+    //   key={`summary #${item.id}`}
+    // >
+    //   <Icon type="font-awesome" name="file" tvParallaxProperties={undefined} />
+    //   <ListItem.Content>
+    //     <ListItem.Title>{item.title}</ListItem.Title>
+    //   </ListItem.Content>
+    //   <Avatar
+    //     // title={item.title[0]}
+    //     // titleStyle={{ color: "black" }}
+    //     source={item.logo_uri && { uri: item.logo_uri }}
+    //     containerStyle={{
+    //       /*borderColor: "green",*/ borderWidth: 1,
+    //       padding: 3,
+    //     }}
+    //   />
+    // </ListItem>
+    <NewsRow
+      item={item}
       onPress={() => {
         navigation.navigate("NewsView", { data: item });
       }}
-      key={`summary #${item.id}`}
-    >
-      <Icon type="font-awesome" name="file" tvParallaxProperties={undefined} />
-      <ListItem.Content>
-        <ListItem.Title>{item.title}</ListItem.Title>
-      </ListItem.Content>
-      <Avatar
-        // title={item.title[0]}
-        // titleStyle={{ color: "black" }}
-        source={item.logo_uri && { uri: item.logo_uri }}
-        containerStyle={{
-          /*borderColor: "green",*/ borderWidth: 1,
-          padding: 3,
-        }}
-      />
-    </ListItem>
+      onLongPress={() => {}}
+      onSwipeLeft={(id: number) => {}}
+    />
   );
 
   const getProfileData = (user_id: number) => {
@@ -186,28 +196,27 @@ export default function AuthorViewScreen(props: any) {
           <View
             style={{ flexDirection: "row", alignItems: "center", padding: 10 }}
           >
-            <Text style={{ fontSize: 16, fontWeight: "700" }}>
-              Trusted sources:{" "}
+            <Text style={{ fontSize: 16, fontWeight: "700", marginRight: 10 }}>
+              Trusted sources:
             </Text>
-            <ScrollView contentContainerStyle={{ flexDirection: "row" }}>
-              {userData.trusted_sources.map((source: any) => (
+            {userData.trusted_sources
+              .filter((source: Source) => !!source.logo_uri)
+              .map((source: Source) => (
                 <Image
-                  source={
-                    source.logo_uri && {
-                      uri: source.logo_uri,
-                    }
-                  }
+                  // title={item.title[0]}
+                  // titleStyle={{ color: "black" }}
+                  source={(source.logo_uri as any) && { uri: source.logo_uri }}
                   style={{
-                    // width: 30,
-                    // height: 30,
-                    borderWidth: 2,
-                    // borderColor: "#6AA84F",
-                    marginHorizontal: 2,
+                    // borderColor: "green",
+                    // borderWidth: 1,
+                    // padding: 3,
+                    height: 34,
+                    width: 34,
+                    resizeMode: "contain",
                   }}
                   key={`trusted source #${source.id}`}
                 />
               ))}
-            </ScrollView>
           </View>
         )}
         <FlatList
