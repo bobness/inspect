6fdddb2e80275065173b7f5c3366f6ae79da9d89
Bob Stark <bob@datagotchi.net>
2022-12-06 17:32:18 -0800

    fixup! Enable search throughout the UI

--- src/components/CommentRow.tsx
+++ src/components/CommentRow.tsx
@@ -22,35 +22,58 @@ const CommentRow = ({ item, navigation }: Props) => {
       key={`comment #${item.id}`}
     >
       <View style={{ flex: 1, flexDirection: "row", marginLeft: TAB_SIZE }}>
-        <Avatar
-          // title={newsData?.title[0]}
-          // titleStyle={{ color: "black" }}
-          source={
-            (item.avatar_uri as any) && {
-              uri: item.avatar_uri,
+        {item.avatar_uri && (
+          <Avatar
+            // title={newsData?.title[0]}
+            // titleStyle={{ color: "black" }}
+            source={
+              (item.avatar_uri as any) && {
+                uri: item.avatar_uri,
+              }
             }
-          }
-          containerStyle={{
-            marginRight: 5,
-          }}
-          // containerStyle={{
-          //   borderColor: "green",
-          //   borderWidth: 1,
-          //   padding: 3,
-          // }}
-          onPress={() => {
-            navigation.navigate("AuthorView", {
-              data: { id: item.user_id },
-            });
-          }}
-        />
+            containerStyle={{
+              marginRight: 5,
+            }}
+            // containerStyle={{
+            //   borderColor: "green",
+            //   borderWidth: 1,
+            //   padding: 3,
+            // }}
+            onPress={() => {
+              navigation.navigate("AuthorView", {
+                data: { id: item.user_id },
+              });
+            }}
+          />
+        )}
+        {!item.avatar_uri && (
+          <Text
+            style={{
+              width: 34,
+              height: 34,
+              borderWidth: 1,
+              borderColor: "black",
+              fontSize: 26,
+              marginRight: 5,
+              textAlign: "center",
+            }}
+            onPress={() => {
+              navigation.navigate("AuthorView", {
+                data: { id: item.user_id },
+              });
+            }}
+          >
+            ?
+          </Text>
+        )}
         <AutoHeightWebView
           style={{
             width:
               Dimensions.get("window").width -
               TAB_SIZE -
               AVATAR_WIDTH -
-              LIST_PADDING,
+              LIST_PADDING -
+              2, // TODO: why is -2 necessary?
           }}
           files={[
             {
