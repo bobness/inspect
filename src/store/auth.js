42a35b57aee8fbcb2c8df9035c2a52381bf2e698
Bob Stark <bob@datagotchi.net>
2024-09-07 21:54:34 -0400

    Updated yarn dependencies for android build

--- src/store/auth.js
+++ src/store/auth.js
@@ -3,41 +3,39 @@ import { instance } from "./api";
 // TODO: remove the res.data thens because that breaks debugging
 
 const userLogin = (data) => {
-  return instance.post("/api/login", data);
+  return instance.post("/login", data);
 };
 
 const userRegister = (data) => {
-  return instance.post("/api/register", data).then((res) => res.data);
+  return instance.post("/register", data).then((res) => res.data);
 };
 
 const userLogout = () => {
-  return instance.post("/api/logout");
+  return instance.post("/logout");
 };
 
 const getAuthUser = () => {
-  return instance.get("/api/users");
+  return instance.get("/users");
 };
 
 const getProfileInformation = (userId) => {
-  return instance.get("/api/users/" + userId).then((res) => res.data);
+  return instance.get("/users/" + userId).then((res) => res.data);
 };
 
 const updateProfile = (data) => {
-  return instance.put("/api/users", data).then((res) => res.data);
+  return instance.put("/users", data).then((res) => res.data);
 };
 
 const deleteAccount = (userId) => {
-  return instance.delete("/api/users/" + userId);
+  return instance.delete("/users/" + userId);
 };
 
 const updateUserExpoToken = (expo_token) => {
-  return instance
-    .put("/api/notification", { expo_token })
-    .then((res) => res.data);
+  return instance.put("/notification", { expo_token }).then((res) => res.data);
 };
 
 const resetPassword = (email) => {
-  return instance.post("/api/reset-password", { email });
+  return instance.post("/reset-password", { email });
 };
 
 export {
--- src/store/news.js
+++ src/store/news.js
@@ -1,102 +1,100 @@
 import { instance } from "./api";
 
 const getUnreadNews = () => {
-  return instance.get(`/api/unread_summaries`);
+  return instance.get(`/unread_summaries`);
 };
 
 const markAsRead = (id) => {
-  return instance.put(`/api/unread_summaries/${id}`).then((res) => res.data);
+  return instance.put(`/unread_summaries/${id}`).then((res) => res.data);
 };
 
 const getAllNews = () => {
-  return instance.get("/api/summaries").then((res) => res.data);
+  return instance.get("/summaries").then((res) => res.data);
 };
 
 const searchSummaries = (keyword) => {
   return instance
-    .get("/api/search/summaries?keyword=" + keyword)
+    .get("/search/summaries?keyword=" + keyword)
     .then((res) => res.data);
 };
 
 const searchUsers = (keyword) => {
   return instance
-    .get("/api/search/users?keyword=" + keyword)
+    .get("/search/users?keyword=" + keyword)
     .then((res) => res.data);
 };
 
 const getNewsById = (newsId) => {
-  return instance.get("/api/summaries/id/" + newsId).then((res) => res.data);
+  return instance.get("/summaries/id/" + newsId).then((res) => res.data);
 };
 
 const getNewsByUid = (newsUid) => {
-  return instance.get("/api/summaries/uid/" + newsUid).then((res) => res.data);
+  return instance.get("/summaries/uid/" + newsUid).then((res) => res.data);
 };
 
 const createSummary = (summary) => {
-  return instance.post("/api/summaries", summary).then((res) => res.data);
+  return instance.post("/summaries", summary).then((res) => res.data);
 };
 
 const updateSummary = (id, updateBlock) => {
-  return instance
-    .put(`/api/summaries/${id}`, updateBlock)
-    .then((res) => res.data);
+  return instance.put(`/summaries/${id}`, updateBlock).then((res) => res.data);
 };
 
 const postComment = (commentData) => {
-  return instance.post("/api/comments", commentData);
+  return instance.post("/comments", commentData);
 };
 
 const deleteComment = (commentId) => {
-  return instance.delete(`/api/comments/${commentId}`);
+  return instance.delete(`/comments/${commentId}`);
 };
 
 const postReaction = (reactionData) => {
-  return instance.post("/api/reactions", reactionData);
+  return instance.post("/reactions", reactionData);
 };
 
 const deleteSummary = (summaryId) => {
-  return instance.delete(`/api/summaries/${summaryId}`);
+  return instance.delete(`/summaries/${summaryId}`);
 };
 
 const getSuggestAuthors = () => {
-  return instance.get("/api/summaries/suggest-authors").then((res) => res.data);
+  return instance.get("/summaries/suggest-authors").then((res) => res.data);
 };
 
 const followAuthor = (followerData) => {
-  return instance.post("/api/followers", followerData);
+  return instance.post("/followers", followerData);
 };
 
 const unfollowAuthor = (authorId) => {
-  return instance.delete(`/api/followers/${authorId}`);
+  return instance.delete(`/followers/${authorId}`);
 };
 
 const blockUser = (userId) => {
-  return instance.post("/api/blocks", { user_id: userId });
+  return instance.post("/blocks", { user_id: userId });
 };
 
 const unblockUser = (userId) => {
-  return instance.delete("/api/blocks/" + userId);
+  return instance.delete("/blocks/" + userId);
 };
 
 const sendNotification = (data) => {
-  return instance.post("/api/notification", data).then((res) => res.data);
+  return instance.post("/notification", data).then((res) => res.data);
 };
 
 const addToDigests = (data) => {
-  return instance.post("/api/digests", data).then((res) => res.data);
+  return instance.post("/digests", data).then((res) => res.data);
 };
 
 const getSource = (baseUrl) => {
-  return instance.get(`/api/sources/${baseUrl}`).then((res) => res.data);
+  return instance.get(`/sources/${baseUrl}`).then((res) => res.data);
 };
 
 const createSource = (baseUrl) => {
-  return instance.post(`/api/sources/${baseUrl}`).then((res) => res.data);
+  return instance.post(`/sources/${baseUrl}`).then((res) => res.data);
 };
 
 const postShare = (summaryId, service, message) => {
   return instance
-    .post("/api/shares", { summaryId, service, message })
+    .post("/shares", { summaryId, service, message })
     .then((res) => res.data);
 };
 
